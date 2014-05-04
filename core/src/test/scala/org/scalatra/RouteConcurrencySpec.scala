f957a7b29386abb8a74ceb95bf88f07f46efc0eb
xuwei-k <6b656e6a69@gmail.com>
2014-05-04 13:09:10 +0900

    fix RouteConcurrencySpec

--- core/src/test/scala/org/scalatra/RouteConcurrencySpec.scala
+++ core/src/test/scala/org/scalatra/RouteConcurrencySpec.scala
@@ -1,25 +1,27 @@
 package org.scalatra
 
 import scala.concurrent._
+import scala.concurrent.duration._
 import ExecutionContext.Implicits.global
 import test.scalatest.ScalatraWordSpec
 
 class RouteConcurrencyServlet extends ScalatraServlet {
-  for {
-    i <- 0 until 250
-    x = Future { get(false) { "/"} }
-  } x
+  val getRoutes: Future[Seq[Route]] =
+    Future.sequence((0 until 250).map(i => Future { get(false) { "/"} }))
 
-  val postRoutes = for {
-    i <- 0 until 250
-    x = Future { post(false) { "/"} }
-  } yield x
+  val postRoutes: Future[Seq[Route]] =
+    Future.sequence((0 until 250).map(i => Future { post(false) { "/"} }))
 
-  for {
-    route <- postRoutes.take(250)
-    x = Future { post(false) {}; post(false) {}} // add some more routes while we're removing
-    y = Future { route.foreach { route => removeRoute("POST", route) }}
-  } (x, y)
+  val action = for {
+    route <- postRoutes
+    result <- Future.sequence(for {
+      _route <- route.take(250)
+      x = Future { post(false) {}; post(false) {}} // add some more routes while we're removing
+      y = Future { removeRoute("POST", _route) }
+    } yield x zip y)
+  } yield result
+
+  Await.result(getRoutes zip action, 30.seconds)
 
   get("/count/:method") {
     routes(HttpMethod(params("method"))).size.toString
